4fe25fd8805ce5b9ae0e7e9e1bb3c048064bc3f8
Estevão P. Rolim <estevao.prolim@gmail.com>
2023-08-02 16:31:46 +0100

    finalizadas páginas de conteúdo (desktop)

---
+++ area-do-cliente.php
@@ -0,0 +1,10 @@
+<!-- CONTATO PAGE -->
+<?php
+include 'partials/header.html';
+
+include "pages/contato/hero.html";
+include "pages/contato/form.html";
+include "pages/contato/tel-whats.html";
+
+include 'partials/footer.html';
+?>
